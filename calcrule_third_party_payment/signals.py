caceb57db33c882fc0f8d3a1b368abbbffb4307f
sniedzielski <sniedzielski@soldevelo.com>
2021-11-24 09:43:12 +0100

    OPL-22: put renamed signal name

--- calcrule_third_party_payment/signals.py
+++ calcrule_third_party_payment/signals.py
@@ -5,7 +5,7 @@ from calcrule_third_party_payment.calculation_rule import ThirdPartyPaymentCalcu
 
 def bind_service_signals():
     bind_service_signal(
-        'bill_creation_from_calculation',
+        'signal_after_invoice_module_bill_creation_from_calculation_run_service',
         ThirdPartyPaymentCalculationRule.convert_batch,
         bind_type=ServiceSignalBindType.AFTER
     )
