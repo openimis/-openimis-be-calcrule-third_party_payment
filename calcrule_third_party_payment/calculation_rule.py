bc4e36fa58dae4aa6369b0dfe54807a80ec7b0d0
delcroip <patrick.delcroix@swisstph.ch>
2024-08-17 11:13:29 +0200

    remove signal

--- calcrule_third_party_payment/calculation_rule.py
+++ calcrule_third_party_payment/calculation_rule.py
@@ -2,7 +2,7 @@ import operator
 
 from django.contrib.contenttypes.models import ContentType
 
-from calcrule_third_party_payment.apps import AbsCalculationRule
+from calcrule_third_party_payment.apps import AbsStrategy
 from calcrule_third_party_payment.config import (
     CLASS_RULE_PARAM_VALIDATION,
     DESCRIPTION_CONTRIBUTION_VALUATION,
@@ -37,7 +37,7 @@ from core.models import User
 import logging
 logger = logging.getLogger(__name__)
 
-class ThirdPartyPaymentCalculationRule(AbsCalculationRule):
+class ThirdPartyPaymentCalculationRule(AbsStrategy):
     version = 1
     uuid = "0a1b6d54-eef4-4ee6-ac47-2a99cfa5e9a8"
     calculation_rule_name = "payment: fee for service"
@@ -50,27 +50,7 @@ class ThirdPartyPaymentCalculationRule(AbsCalculationRule):
     type = "account_payable"
     sub_type = "third_party_payment"
 
-    signal_get_rule_name = Signal([])
-    signal_get_rule_details = Signal([])
-    signal_get_param = Signal([])
-    signal_get_linked_class = Signal([])
-    signal_calculate_event = Signal([])
-    signal_convert_from_to = Signal([])
 
-    @classmethod
-    def ready(cls):
-        now = datetime.datetime.now()
-        condition_is_valid = (now >= cls.date_valid_from and now <= cls.date_valid_to) \
-            if cls.date_valid_to else (now >= cls.date_valid_from and cls.date_valid_to is None)
-        if condition_is_valid:
-            if cls.status == "active":
-                # register signals getParameter to getParameter signal and getLinkedClass ot getLinkedClass signal
-                cls.signal_get_rule_name.connect(cls.get_rule_name, dispatch_uid="on_get_rule_name_signal")
-                cls.signal_get_rule_details.connect(cls.get_rule_details, dispatch_uid="on_get_rule_details_signal")
-                cls.signal_get_param.connect(cls.get_parameters, dispatch_uid="on_get_param_signal")
-                cls.signal_get_linked_class.connect(cls.get_linked_class, dispatch_uid="on_get_linked_class_signal")
-                cls.signal_calculate_event.connect(cls.run_calculation_rules, dispatch_uid="on_calculate_event_signal")
-                cls.signal_convert_from_to.connect(cls.run_convert, dispatch_uid="on_convert_from_to")
 
     @classmethod
     def active_for_object(cls, instance, context, type='account_payable', sub_type='third_party_payment'):
